18d64ababb6662fcbcd3f787f7a1ab3d748cbc9f
Alex <htmldav@mail.ru>
2021-07-10 08:48:20 +0300

    color var()

--- sliderActive.js
+++ sliderActive.js
@@ -1,35 +1,41 @@
 const sliderControls = document.querySelector(".slider-controls");
 
-const currentClassName = 'current';
-const slideCurrentClassName = 'slide-current';
-const featuresListClassName = 'features-list';
-const sliderControlsButtonsClass = 'slider-controls';
+const currentClassName = "current";
+const slideCurrentClassName = "slide-current";
+const featuresListClassName = "features-list";
+const sliderControlsButtonsClass = "slider-controls";
 
-const sliderControlsButtons = document.getElementsByClassName(sliderControlsButtonsClass)[0];
+const sliderControlsButtons = document.getElementsByClassName(
+  sliderControlsButtonsClass
+)[0];
 
 const featuresList = document.getElementsByClassName(featuresListClassName)[0];
-
 
 sliderControls.addEventListener("click", function (evt) {
   evt.preventDefault();
 
-  if (evt.target.nodeName != 'BUTTON') {
-    return
+  if (evt.target.nodeName != "BUTTON") {
+    return;
   }
 
   const newCurentElementClassName = evt.target.className;
 
-  let curentElement = sliderControlsButtons.getElementsByClassName(currentClassName)[0];
+  let curentElement =
+    sliderControlsButtons.getElementsByClassName(currentClassName)[0];
   curentElement.classList.remove(currentClassName);
 
-  let newCurentElement = sliderControlsButtons.getElementsByClassName(newCurentElementClassName)[0];
+  let newCurentElement = sliderControlsButtons.getElementsByClassName(
+    newCurentElementClassName
+  )[0];
   newCurentElement.classList.add(currentClassName);
 
+  let featuresListCurrentElement = featuresList.getElementsByClassName(
+    newCurentElementClassName
+  )[0];
 
-
-  let featuresListCurrentElement = featuresList.getElementsByClassName(newCurentElementClassName)[0];
-
-  featuresList.getElementsByClassName(slideCurrentClassName)[0].classList.remove(slideCurrentClassName);
+  featuresList
+    .getElementsByClassName(slideCurrentClassName)[0]
+    .classList.remove(slideCurrentClassName);
 
   featuresListCurrentElement.classList.add(slideCurrentClassName);
 });
